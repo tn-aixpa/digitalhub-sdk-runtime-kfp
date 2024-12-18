5e484a3ff0b8ce615c49aa71f8647d2c3399a4bf
EvangMM <matteomartini92@yahoo.it>
2024-12-18 16:14:28 +0100

    fix: correct zip+https handling

--- digitalhub_runtime_kfp/utils/configurations.py
+++ digitalhub_runtime_kfp/utils/configurations.py
@@ -93,13 +93,13 @@ def save_workflow_source(path: Path, source_spec: dict) -> str:
 
     # Http(s) or s3 presigned urls
     elif has_remote_scheme(source):
+        filename = path / get_filename_from_uri(source)
         if has_zip_scheme(source):
-            filename = path / get_filename_from_uri(source)
-            source = source.removeprefix("zip+")
+            requests_chunk_download(source.removeprefix("zip+"), filename)
+            extract_archive(path, filename)
+            filename.unlink()
         else:
-            filename = path / "archive.zip"
-        requests_chunk_download(source, filename)
-        extract_archive(path, filename)
+            requests_chunk_download(source, filename)
 
     # S3 path
     elif has_s3_scheme(source):
@@ -109,6 +109,7 @@ def save_workflow_source(path: Path, source_spec: dict) -> str:
         bucket, key = get_bucket_and_key(source)
         get_s3_source(bucket, key, filename)
         extract_archive(path, filename)
+        filename.unlink()
 
     # Unsupported scheme
     else:
