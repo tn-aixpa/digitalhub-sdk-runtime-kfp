53f55b56e12b705bf20b4ecf611d6ad76774390c
EvangMM <matteomartini92@yahoo.it>
2025-06-12 09:54:11 +0200

    docs: added copyright header

--- digitalhub_runtime_kfp/__init__.py
+++ digitalhub_runtime_kfp/__init__.py
@@ -1,3 +1,5 @@
+# SPDX-FileCopyrightText: © 2025 DSLab - Fondazione Bruno Kessler
+#
 # SPDX-License-Identifier: Apache-2.0
 from digitalhub_runtime_kfp.entities._commons.enums import EntityKinds
 from digitalhub_runtime_kfp.entities.run.kfp_run.builder import RunKfpRunBuilder
